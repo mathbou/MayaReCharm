12b4dce75b3275a07777a8324e9e0ce50852104c
Mathieu <923463-mathbou@users.noreply.gitlab.com>
2025-04-18 23:24:10 +0200

    ⬆️ migration to gradle 8 + kotlin 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,99 +1,113 @@
-import org.jetbrains.changelog.closure
 import org.jetbrains.changelog.markdownToHTML
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.changelog.Changelog
 
 plugins {
-    id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.4.32"
-    id("org.jetbrains.intellij") version "0.7.2"
-    id("org.jetbrains.changelog") version "1.1.2"
+    kotlin("jvm") version "2.1.20"
+    id("org.jetbrains.intellij.platform") version "2.5.0"
+    id("org.jetbrains.changelog") version "2.2.1"
 }
 
-val pluginGroup: String by project
-val pluginName_: String by project
-val pluginVersion: String by project
+group = providers.gradleProperty("pluginGroup").get()
+version = providers.gradleProperty("pluginVersion").get()
 
-val pluginSinceVersion: String by project
-val pluginUntilVersion: String by project
-val pluginVerifierIdeVersions: String by project
-
-val platformType: String by project
-val platformVersion: String by project
-val platformDownloadSources: String by project
-val platformPlugins: String by project
-
-val publishToken: String? = System.getenv("IDEA_PLATFORM_PUBLISH")
-
-group = pluginGroup
-version = pluginVersion
+// Set the JVM language level used to build the project.
+kotlin {
+    jvmToolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+        vendor = JvmVendorSpec.JETBRAINS
+    }
+}
 
 repositories {
     mavenCentral()
-    jcenter()
+
+    intellijPlatform {
+        defaultRepositories()
+        jetbrainsRuntime()
+    }
 }
 
 dependencies {
+    intellijPlatform {
+        val type = providers.gradleProperty("platformType")
+        val version = providers.gradleProperty("platformVersion")
+        create(type, version, useInstaller = false)
+
+        pluginVerifier()
+
+        // Plugin Dependencies. Uses `platformBundledPlugins` property from the gradle.properties file for bundled IntelliJ Platform plugins.
+        bundledPlugins(providers.gradleProperty("platformBundledPlugins").map { it.split(',') })
+
+    }
 }
 
-// See https://github.com/JetBrains/gradle-intellij-plugin/
-intellij {
-    pluginName = pluginName_
-    version = platformVersion
-    type = platformType
-    downloadSources = platformDownloadSources.toBoolean()
-    updateSinceUntilBuild = true
-    setPlugins(*platformPlugins.split(",").map(String::trim).filter(String::isNotEmpty).toTypedArray())
+intellijPlatform {
+
+    pluginConfiguration{
+        id = providers.gradleProperty("pluginGroup")
+        name = providers.gradleProperty("pluginName_")
+        version = providers.gradleProperty("pluginVersion")
+
+        ideaVersion {
+            sinceBuild = providers.gradleProperty("pluginSinceVersion")
+//            untilBuild = providers.gradleProperty("pluginUntilVersion")
+        }
+
+        description = providers.fileContents(layout.projectDirectory.file("README.md")).asText.map {
+            val start = "<!-- Plugin description -->"
+            val end = "<!-- Plugin description end -->"
+
+            with(it.lines()) {
+                if (!containsAll(listOf(start, end))) {
+                    throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
+                }
+                subList(indexOf(start) + 1, indexOf(end)).joinToString("\n").let(::markdownToHTML)
+            }
+        }
+
+        val changelog = project.changelog // local variable for configuration cache compatibility
+        // Get the latest available change notes from the changelog file
+        changeNotes = providers.gradleProperty("pluginVersion").map { pluginVersion ->
+            with(changelog) {
+                renderItem(
+                    (getOrNull(pluginVersion) ?: getUnreleased())
+                        .withHeader(false)
+                        .withEmptySections(false),
+                    Changelog.OutputType.HTML,
+                )
+            }
+        }
+    }
+
+    publishing {
+        token = System.getenv("IDEA_PLATFORM_PUBLISH")
+        channels = providers.gradleProperty("pluginVersion").map { listOf(it.substringAfter('-', "").substringBefore('.').ifEmpty { "default" }) }
+    }
+
+    pluginVerification {
+        ides {
+            recommended()
+        }
+    }
 }
 
 changelog {
-    version = pluginVersion
+    version = providers.gradleProperty("pluginVersion")
     groups = emptyList()
 }
 
 tasks {
+    wrapper {
+        gradleVersion = providers.gradleProperty("gradleVersion").get()
+    }
+
     withType<JavaCompile> {
-        options.release.set(8)
-        sourceCompatibility = "1.8"
-        targetCompatibility = "1.8"
-    }
-
-    withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "1.8"
-    }
-
-    patchPluginXml {
-        version(pluginVersion)
-        sinceBuild(pluginSinceVersion)
-        untilBuild(null)
-
-        pluginDescription(
-            closure {
-                File(projectDir, "README.md").readText().lines().run {
-                    val start = "<!-- Plugin description -->"
-                    val end = "<!-- Plugin description end -->"
-
-                    if (!containsAll(listOf(start, end))) {
-                        throw GradleException("Plugin description section not found in README.md\n$start ... $end")
-                    }
-                    subList(indexOf(start) + 1, indexOf(end))
-                }.joinToString("\n").run { markdownToHTML(this) }
-            }
-        )
-
-        changeNotes(
-            closure {
-                changelog.get(pluginVersion).toHTML()
-            }
-        )
-    }
-
-    runPluginVerifier {
-        ideVersions(pluginVerifierIdeVersions)
+        options.release = 21
+        sourceCompatibility = "21"
+        targetCompatibility = "21"
     }
 
     publishPlugin {
         dependsOn("patchChangelog")
-        token(publishToken)
-        channels(pluginVersion.split('-').getOrElse(1) {"default"}.split('.').first())
     }
 }
