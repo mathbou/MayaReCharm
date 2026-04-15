5191e6e51eac588994ec8941be07034d18f61f41
Mathieu <13415583+mathbou@users.noreply.github.com>
2026-04-15 15:12:24 +0200

    ♻ Refactor LogConsole and LogWindow: enhance log handling, add reconnect action, and improve SDK tab management

--- src/main/kotlin/logconsole/LogConsole.kt
+++ src/main/kotlin/logconsole/LogConsole.kt
@@ -8,27 +8,44 @@ import com.intellij.execution.process.ProcessOutputTypes
 import com.intellij.openapi.actionSystem.ActionGroup
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.Constraints
 import com.intellij.openapi.actionSystem.DefaultActionGroup
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.IconLoader
 import com.intellij.psi.search.GlobalSearchScope
+import mayacomms.MayaCommandInterface
 import java.io.File
 import java.nio.charset.Charset
+import MayaBundle as Loc
 
-
-class MyCustomClearConsoleAction(customConsole: LogConsole?) :
+class ClearConsoleAndLogFileAction(private val logConsole: LogConsole) :
     ClearConsoleAction() {
-    private val myCustomConsole: LogConsole? = customConsole
 
     override fun actionPerformed(e: AnActionEvent) {
         super.actionPerformed(e)
-        myCustomConsole?.clearFile()
+        logConsole.clearLogFile()
     }
 }
 
+class ConnectToMayaLogAction(
+    private val logConsole: LogConsole,
+    private val port: Int
+) : AnAction(
+    Loc.message("mayarecharm.action.ConnectLogText"),
+    Loc.message("mayarecharm.action.ConnectLogDescription"),
+    IconLoader.getIcon("/icons/MayaReCharm_Action.png", ConnectToMayaLogAction::class.java)
+) {
+
+    override fun actionPerformed(e: AnActionEvent) {
+        MayaCommandInterface(port).connectMayaLog()
+        logConsole.reloadLogFile()
+    }
+}
 
 class LogConsole(
     private val project: Project,
     private val file: File,
+    private val port: Int,
     charset: Charset,
     skippedContents: Long,
     title: String,
@@ -46,34 +63,40 @@ class LogConsole(
     }
 
     override fun addMessage(text: String?) {
-        var text_: String = text ?: ""
-        val logType = LogConsolePreferences.getType(text_)
+        var msg: String = text ?: ""
+        val logType = LogConsolePreferences.getType(msg)
 
         if (logType?.startsWith("WARN") ?: false) {
-            text_ = "\u001B[93m$text_\u001B[0m"
+            msg = "\u001B[93m$msg\u001B[0m"
         }
         else if (logType?.startsWith("DEBUG") ?: false) {
-            text_ = "\u001B[96m$text_\u001B[0m"
+            msg = "\u001B[96m$msg\u001B[0m"
         }
 
-        super.addMessage(text_)
+        super.addMessage(msg)
     }
 
     override fun isActive(): Boolean {
         return true
     }
 
-    fun clearFile() {
+    fun clearLogFile() {
         file.writeText("")
     }
 
-    override fun getOrCreateActions(): ActionGroup? {
+    fun reloadLogFile() {
+        activate()
+    }
+
+    override fun getOrCreateActions(): ActionGroup {
         val group: DefaultActionGroup = super.getOrCreateActions() as DefaultActionGroup
 
-        val clearAllAction = MyCustomClearConsoleAction(this)
+        val clearAllAction = ClearConsoleAndLogFileAction(this)
         val originalClearAllAction: AnAction = group.childActionsOrStubs.last { it is ClearConsoleAction }
 
         group.replaceAction(originalClearAllAction, clearAllAction)
+
+        group.addAction(ConnectToMayaLogAction(this, port), Constraints.FIRST)
 
         return group as ActionGroup
     }
